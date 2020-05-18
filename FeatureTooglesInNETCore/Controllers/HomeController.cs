ec33c9dee527ab1cb25f20daf029c112a1d828ff
Piotr Wachulec <p.wachulec@gmail.com>
2020-05-18 23:04:59 +0200

    Created HomeController with example actions

---
+++ FeatureTooglesInNETCore/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.Mvc;
+
+namespace FeatureTooglesInNETCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly IFeatureManager _featureManager;
+
+        public HomeController(IFeatureManager featureManager)
+        {
+            _featureManager = featureManager;
+        }
+        
+        [HttpGet("FeatureA")]
+        [FeatureGate(MyFeatureFlags.FeatureA)]
+        public IActionResult GetA()
+        {
+            return Ok("Home controller works!");
+        }
+
+        [HttpGet("FeatureB")]
+        [ApiExplorerSettings(IgnoreApi = false)]
+        public IActionResult GetB()
+        {
+            return Ok("Home controller works!");
+        }
+
+        [HttpGet("FeatureC")]
+        [FeatureGate(MyFeatureFlags.FeatureC)]
+        public IActionResult GetC()
+        {
+            return Ok("Feature C works");
+        }
+    }
+}
